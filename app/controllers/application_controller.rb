210b0a78e5f060841a71817b99f902c2eadbf117
laushinka <laurie.malau@gmail.com>
2016-06-27 09:33:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
+# The controllers are where the application configurations,
+# routes, and controller actions are implemented.
+# Controllers represent the application logic, generally,
+# the interface and flow of our application.
 class ApplicationController < Sinatra::Base
 
+# This configure block tells the controller where to look to find the views
+# and the public directory
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
--- config.ru
+++ config.ru
@@ -1,3 +1,10 @@
+# A config.ru file is necessary when building Rack-based applications
+# and using rackup/shotgun to start the application server
+# (the ru stands for rackup)
+
+# first responsible for loading application environment, code, and libraries.
 require_relative './config/environment'
 
+# Once all code is loaded, config.ru then specifies which controllers to load
+# as part of our application, using run or use.
 run ApplicationController
